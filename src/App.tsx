1c22ac74397ef393ecd6bdc67cc3a72e587ad403
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-07-01 15:27:03 +0100

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import { tab } from '@testing-library/user-event/dist/tab';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Stop from './classes/Stop';
 import Bus from './classes/Bus';
 
-const getLongLatFromPostcode = async (postcode : string) : Promise<{long:number, lat:number}> =>{
+const getLongLatFromPostcode = async (postcode: string): Promise<{ long: number, lat: number }> => {
   const url = new URL(`https://api.postcodes.io/postcodes/${postcode}`)
   const latLongRes = await fetch(url);
-  if(!latLongRes.ok)
-  {
+  if (!latLongRes.ok) {
     throw new Error("Request to api.postcodes.io was not successful!")
   }
   const latLongData = await latLongRes.json();
   const long = latLongData.result.longitude;
   const lat = latLongData.result.latitude;
-  return {long, lat}
+  return { long, lat }
 }
 
-const getStopsFromLongLat = async (long :number, lat:number) : Promise<any>=> {
+const getStopsFromLongLat = async (long: number, lat: number): Promise<any> => {
   // https://api-portal.tfl.gov.uk/api-details#api=StopPoint&operation=StopPoint_GetByGeoPointByQueryLatQueryLonQueryStopTypesQueryRadiusQueryUseSt
-  const stopRes = await fetch("https://api.tfl.gov.uk/StopPoint/?lat="+lat+"&lon="+long+"&stopTypes=NaptanPublicBusCoachTram&radius=300");
-  if(!stopRes.ok)
-  {
+  const stopRes = await fetch("https://api.tfl.gov.uk/StopPoint/?lat=" + lat + "&lon=" + long + "&stopTypes=NaptanPublicBusCoachTram&radius=300");
+  if (!stopRes.ok) {
     throw new Error("Request to api.tfl.gov.uk/StopPoint was not successful!")
   }
   const stopData = await stopRes.json()
@@ -29,29 +27,32 @@ const getStopsFromLongLat = async (long :number, lat:number) : Promise<any>=> {
 
 
 async function getStops(postcode: string): Promise<Stop[]> {
-  
-    const {long, lat} = await getLongLatFromPostcode(postcode);
-    const stops = await getStopsFromLongLat(long, lat);
-    
-    let busStops:Stop[] = [];
-
-    stops.stopPoints.forEach(async (item:any)=>{
-
-      const currentStop = item.naptanId
-      const stopRes = await fetch(`https://api.tfl.gov.uk/StopPoint/${currentStop}/Arrivals`);
-      const arrivalPredictions = await stopRes.json()
-      let buses:Bus[] = [];
-      arrivalPredictions.forEach((bus:any) => {
-        buses.push( new Bus(bus.destinationName,bus.lineName,bus.expectedArrival,bus.id));
-      });
-
-      busStops.push(new Stop(item.naptanId,item.lat,item.lon,item.commonName,"",buses))
-
-    })
-// TODO: add in stop code
 
 
-    return busStops;
+  //return [new Stop("ui",9,9,"d","wd",[])]
+  const { long, lat } = await getLongLatFromPostcode(postcode);
+  const stops = await getStopsFromLongLat(long, lat);
+
+  let busStops: Stop[] = [];
+
+  stops.stopPoints.forEach(async (item: any) => {
+
+    const currentStop = item.naptanId
+    const stopRes = await fetch(`https://api.tfl.gov.uk/StopPoint/${currentStop}/Arrivals`);
+    const arrivalPredictions = await stopRes.json()
+    let buses: Bus[] = [];
+    arrivalPredictions.forEach((bus: any) => {
+      buses.push(new Bus(bus.destinationName, bus.lineName, bus.expectedArrival, bus.id));
+    });
+
+    busStops.push(new Stop(item.naptanId, item.lat, item.lon, item.commonName, "", buses))
+
+  })
+  // TODO: add in stop code
+
+
+
+  return busStops;
 }
 
 
@@ -60,37 +61,37 @@ function App(): React.ReactElement {
 
 
   const [postcode, setPostcode] = useState<string>("");
-    const [tableData, setTableData] = useState<Stop[]>([]);
+  const [tableData, setTableData] = useState<Stop[]>([]);
 
 
 
-    async function formHandler(event: React.FormEvent<HTMLFormElement>): Promise<void> {
-        event.preventDefault(); // to stop the form refreshing the page when it submits
-        const data = await getStops(postcode);
-        setTableData(data);
-    }
+  async function formHandler(event: React.FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault(); // to stop the form refreshing the page when it submits
+    const data = await getStops(postcode);
+    setTableData(data);
+  }
 
-    function updatePostcode(data: React.ChangeEvent<HTMLInputElement>): void {
-        setPostcode(data.target.value)
-    }
+  function updatePostcode(data: React.ChangeEvent<HTMLInputElement>): void {
+    setPostcode(data.target.value)
+  }
 
-    return <>
-        <h1> BusBoard </h1>
-        <form action="" onSubmit={formHandler}>
-            <label htmlFor="postcodeInput"> Postcode: </label>
-            <input type="text" id="postcodeInput" onChange={updatePostcode}/>
-            <input type="submit" value="Submit"/>
-        </form>
-        <ol>
+  return <>
+    <h1> BusBoard </h1>
+    <form action="" onSubmit={formHandler}>
+      <label htmlFor="postcodeInput"> Postcode: </label>
+      <input type="text" id="postcodeInput" onChange={updatePostcode} />
+      <input type="submit" value="Submit" />
+    </form>
+    <ol>
 
-        {
-        tableData.map((stop:Stop)=>
-           <li key={stop.ID}>{stop.name}</li>
+      {
+        tableData.map((stop: Stop) =>
+          <li key={stop.ID}>{stop.name}</li>
         )
       }
-        </ol>
+    </ol>
 
-    </>;
+  </>;
 }
 
 export default App;
