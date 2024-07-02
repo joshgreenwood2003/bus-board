87398a31ddd634de7c2d3a684a9d81b35100e8d3
Alan Prophett <alan.prophett@softwire.com>
2024-07-02 14:42:10 +0100

    pop up styling and unique route colours

--- src/classes/Bus.ts
+++ src/classes/Bus.ts
@@ -1,3 +1,23 @@
+const seedrandom = require('seedrandom')
+
+function getRgbFromInputString(inputString:string) : [number,number,number]{
+    // Generate a unique number from input string
+    let seed:number = 0
+    let weight = 10;
+    for(let i = 0; i < inputString.length; i++){
+        seed += inputString.charCodeAt(i)*weight
+        weight+=10
+    }
+
+    const random = new seedrandom(seed);
+
+    // Weighted more towards green and all above a certain threshold to avoid dark colors
+    const r = Math.floor(random() * 150) + 85;
+    const g = Math.floor(random() * 200) + 45;
+    const b = Math.floor(random() * 100) + 135;
+
+    return [r, g, b];
+}
 
 
 export default class Bus{
@@ -6,6 +26,7 @@ export default class Bus{
     public arrivalTime:Date;
     public ID:string
     public timeToStation:number
+    public color: [number, number, number]
 
     public constructor(_destination:string,_line:string,_arrivalTime:string,_ID:string,_TTS:number){
         this.destination = _destination;
@@ -13,5 +34,9 @@ export default class Bus{
         this.arrivalTime = new Date(_arrivalTime);
         this.ID = _ID;
         this.timeToStation = _TTS
+        const rgbValues = getRgbFromInputString(_line);
+        this.color = rgbValues
     }
+
+    
 }
