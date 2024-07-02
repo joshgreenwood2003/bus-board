62fe15629b267fedbf1f9eb58087f5490ec76708
Josh Greenwood <31244208+joshgreenwood2003@users.noreply.github.com>
2024-07-02 15:01:07 +0100

    current location (#7)

--- src/components/MapBoxContainer.tsx
+++ src/components/MapBoxContainer.tsx
@@ -1,19 +1,21 @@
 import { MapContainer, MapContainerProps, Marker, Popup, TileLayer, useMap, useMapEvent } from 'react-leaflet'
-import { Icon, LatLng, LatLngExpression, Map } from 'leaflet';
+import { Icon, LatLng, LatLngExpression, Map, marker } from 'leaflet';
 import Stop from '../classes/Stop';
 import BusStop from './BusStop';
 
 interface Prop {
     setMapRef: React.SetStateAction<any>,
-    tableData: Stop[]
+    tableData: Stop[],
+    userPosition:GeolocationPosition | undefined
 
 }
 
 export const ZOOM_DEFAULT_DISTANCE = 16
 
 const BUS_STOP_MAP_MARKER_IMAGE = "https://cdn-icons-png.flaticon.com/512/3448/3448339.png"
-const icon = new Icon({ iconUrl: BUS_STOP_MAP_MARKER_IMAGE, iconSize: [35, 35], iconAnchor: [18, 35], popupAnchor: [0, -1] })
-  
+const LOCATION_MARKER_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Location_dot_blue.svg/640px-Location_dot_blue.svg.png"
+const icon = new Icon({ iconUrl: BUS_STOP_MAP_MARKER_IMAGE, iconSize: [50, 50], iconAnchor: [25, 50], popupAnchor: [0, -1] })
+const LocationIcon = new Icon({ iconUrl: LOCATION_MARKER_IMAGE, iconSize: [25, 25], iconAnchor: [12, 12]})
 const MapBoxContainer = (props: Prop) => {
 
   return (
@@ -28,6 +30,7 @@ const MapBoxContainer = (props: Prop) => {
       </Marker>
       
       )}
+      {props.userPosition && <Marker icon = {LocationIcon}position = {new LatLng(props.userPosition.coords.latitude,props.userPosition.coords.longitude)}></Marker>}
    
   </MapContainer>
   )
